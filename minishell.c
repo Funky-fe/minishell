f360e58773cb03da400cdffeaaf4a4734d666512
Ruben Andre Lima Fernandes <rlima-fe@c1r11s10.42porto.com>
2024-01-26 12:31:39 +0000

    slowly

--- minishell.c
+++ minishell.c
@@ -6,13 +6,25 @@
 /*   By: rlima-fe <rlima-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 19:14:43 by rlima-fe          #+#    #+#             */
-/*   Updated: 2024/01/17 19:14:44 by rlima-fe         ###   ########.fr       */
+/*   Updated: 2024/01/26 11:30:25 by rlima-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "headers/minishell.h"
 
-void	sa_handler(int sig, siginfo_t *info, void *context)
+static char	*get_input(t_mini *ms, const char *prompt)
+{
+	char	*input;
+	
+	input = readline(prompt);
+	if (input && input[0])
+		//command history function
+	else if (!input)
+		exit_handler(ms, EXIT_MSG, 0);
+	return (input);
+}
+
+void	ft_sa_handler(int sig, siginfo_t *info, void *context)
 {
 	(void)context;
 	ft_printf("sig: %d\n", sig);
@@ -25,21 +37,37 @@ void	sa_handler(int sig, siginfo_t *info, void *context)
 	}
 }
 
-static t_mini	init_ms(int ac, char **av, char **env)
+static t_mini	ft_init(int argc, char *argv[], char **envp)
 {
 	t_mini				ms;
 	struct sigaction	sa;
 
-	ft_bzero(&ms, sizeof (t_mini));
-	if (ac > 1)
-		exit_handler(&ms, "Error, use ./minishell" ,1);
-	ft_bzero(&sa, sizeof (sa));
+	(void) argv;
+	ft_bzero (&ms, sizeof (t_mini));
+	if (argc > 1)
+		exit_handler (&ms, "Usage: ./minishell", 1);
+	ft_bzero (&sa, sizeof (sa));
 	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ft_sa_handler;
+	sigaction (SIGINT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
+	ms.envp = ft_matdup (envp);
+	if (!envp)
+		exit_handler (&ms, "Error allocating memory to environment", 1);
+	ms.prompt = ft_strdup("minishell > ");
+	if (!ms.prompt)
+		exit_handler (&ms, "Error allocating memory to prompt", 1);
+	return (ms);
 }
 
 int	main(int argc, char **argv, char **env)
 {
 	t_mini	ms;
 
-	ms = init_ms(argc, argv, env);
+	ms = ft_init(argc, argv, env);
+	while (1)
+	{
+		ms.input = get_input(&ms, ms.prompt);
+		if (ms.input && *ms.input)
+	}
 }
