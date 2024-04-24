6b0caa146bd263cc0c7477e42255d65f2fe3e7ff
Rlima-fe <funky@Funky>
2024-04-24 15:42:09 +0100

    builtins completed

--- sources/builtin/builtin.c
+++ sources/builtin/builtin.c
@@ -1,18 +1,23 @@
 #include "minishell.h"
 
-int execbi(t_mini *ms, char **cmd, char ***envp)
+int execbi(t_mini *ms, char **cmd, char **envp)
 {
     if (!ms || !cmd || !envp)
         return (-1);
     if (!ft_strncmp(*cmd, "echo", 5))
         ft_echo(ms, cmd);
     else if (!ft_strncmp(*cmd, "cd", 3))
+        ft_cd(ms, cmd, envp);
     else if (!ft_strncmp(*cmd, "pwd", 4))
-        
+        ft_pwd(ms, cmd, envp);
     else if (!ft_strncmp(*cmd, "export", 7))
+        ft_export(ms, cmd, envp);
     else if (!ft_strncmp(*cmd, "unset", 6))
+        ft_unset(ms, cmd, envp);
     else if (!ft_strncmp(*cmd, "env", 4))
+        ft_env(ms, cmd, envp);
     else if (!ft_strncmp(*cmd, "exit", 5))
+        ft_exit(ms, cmd);
     else
         return (0);
     return (1);
